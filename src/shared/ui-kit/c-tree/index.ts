086b27ff07660eca2b30d2c1ab88c75db74a9a3a
XoldpandaX <denis.d.shakin@gmail.com>
2026-05-02 17:09:08 +0200

    feat(ui-kit): CTree cmp

---
+++ src/shared/ui-kit/c-tree/index.ts
@@ -0,0 +1,3 @@
+export { CTree } from './c-tree'
+export { getTreeExpandedState, useTree } from '@mantine/core'
+export type { RenderTreeNodePayload, TreeNodeData, TreeProps, UseTreeInput, UseTreeReturnType } from '@mantine/core'
